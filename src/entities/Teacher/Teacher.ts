5510ac4da20c23ef3d71898c5f39468133e9df3a
AlexBujosa <bujosa2014@outlook.com>
2023-01-06 11:36:31 -0400

    Fix: Teacher Field String to Type Major

--- src/entities/Teacher/Teacher.ts
+++ src/entities/Teacher/Teacher.ts
@@ -1,11 +1,14 @@
+import Major from "../../types/Majors";
 import { Person } from "../Person/Person";
 
 export class Teacher extends Person {
-	private readonly major: string;
+	private readonly major: Major;
 
-	constructor(name: string, surnames: string, age: number, major: string) {
+	constructor(name: string, surnames: string, age: number, major: Major) {
 		super(name, surnames, age);
 
 		this.major = major;
+
+
 	}
 }
